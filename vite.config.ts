e873ec328054591346bbdc03443e1d2eb0c42682
lizau <liza.uchaneva@outlook.com>
2026-01-28 12:36:49 -0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/mork-borg-map-builder/" : "",
+  base: mode === "production" ? "/mork-borg-map-builder/" : "/mork-borg-map-builder/",
 }));
